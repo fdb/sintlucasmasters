142ecc47724deb80569678766b48f956a7612b0b
Frederik De Bleser <frederik@debleser.be>
2022-05-15 23:36:09 +0200

    Index page shows grid of images.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,39 @@
 	export let students;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid">
+	{#each students as student}
+		<div class="student">
+			<a href={`/students/${student.student_name.toLowerCase().replace(' ', '-')}`}>
+				<img
+					src={`${student.main_image}/-/smart_resize/600x600/`}
+					alt={student.project_title}
+					width="300"
+					height="300"
+				/>
+				<div class="project-title">{student.project_title}</div>
+				<div class="student-name">{student.student_name}</div></a
+			>
+		</div>
+	{/each}
+</div>
 
-{#each students as student}
-	<div class="student">
-		<a href={`/students/${student.student_name.toLowerCase().replace(' ', '-')}`}>
-			{student.student_name} — {student.project_title}</a
-		>
-	</div>
-{/each}
+<style>
+	.student a {
+		text-decoration: none;
+	}
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		grid-gap: 4rem 1rem;
+		text-decoration: none;
+	}
+	.project-title {
+		font-size: 1rem;
+		font-weight: bold;
+		margin-top: 0.5rem;
+	}
+	.student-name {
+		font-size: 0.8rem;
+	}
+</style>
