ba7420ea04b1534f88193e8568c4545a44ae22b6
Frederik De Bleser <frederik@debleser.be>
2022-05-16 23:52:16 +0200

    Little bit of styling for student detail page.

--- src/routes/students/[name].svelte
+++ src/routes/students/[name].svelte
@@ -8,10 +8,14 @@
 	<title>{attributes.student_name} — {attributes.project_title} | Sint Lucas Masters</title>
 </svelte:head>
 
-<h2>{attributes.student_name} — {attributes.project_title}</h2>
-<p class="year">{attributes.year}</p>
+<div class="meta">
+	<h2>{attributes.student_name} — {attributes.project_title}</h2>
+	<p class="year">{attributes.year}</p>
+</div>
 
-<img src={attributes.main_image} alt={attributes.project_title} />
+<div class="full-width">
+	<img class="main-image" src={attributes.main_image} alt={attributes.project_title} />
+</div>
 
 <article>
 	{@html marked.parse(body)}
@@ -19,14 +23,27 @@
 
 {#if attributes.images}
 	{#each attributes.images as image}
-		<img src={image} alt={attributes.project_title} />
+		<img class="image" src={image} alt={attributes.project_title} />
 	{/each}
 {/if}
 
 <style>
+	.meta {
+		margin-top: 2rem;
+		margin-bottom: 4rem;
+	}
+	.full-width {
+		margin-left: calc(-50vw + 50%);
+		margin-right: calc(-50vw + 50%);
+	}
+	.main-image {
+		max-height: 80vh;
+		width: 100%;
+		object-fit: contain;
+	}
 	article {
-		margin-top: 1rem;
-		margin-bottom: 1rem;
+		max-width: 80ch;
+		margin: 5rem auto;
 	}
 	p {
 		margin-bottom: 1rem;
@@ -36,5 +53,10 @@
 		font-size: 0.8rem;
 		margin-top: 0.5rem;
 		margin-bottom: 0.5rem;
+	}
+
+	.image {
+		display: block;
+		margin: 4rem auto;
 	}
 </style>
