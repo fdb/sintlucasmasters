350706cef8f4dabb4370f63fc0fd97341477738e
Frederik De Bleser <frederik@debleser.be>
2022-05-17 16:40:55 +0200

    Use static prerendering for website.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,3 +4,20 @@
 </script>
 
 <Artboard {students} />
+
+<ul class="student-list-invisible">
+	{#each students as student}
+		<li>
+			<a href={`/students/${student.slug}`}>
+				<h3>{student.student_name}</h3>
+				<p>{student.project_title}</p>
+			</a>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	.student-list-invisible {
+		display: none;
+	}
+</style>
