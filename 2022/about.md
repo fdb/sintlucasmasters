097c120a5c5d5041665fea8fd9d3e15f95320f92
Frederik De Bleser <frederik@debleser.be>
2022-06-06 13:33:14 +0200

    Create some sort of page transition

---
+++ 2022/about.md
@@ -0,0 +1,7 @@
+---
+layout: page
+---
+
+# Matter Matters
+
+The exhibition of the Sint Lucas Antwerpen masters of the academic year 2021-2022 is called "Matter Matters".
--- 2022/static/js/grid.js
+++ 2022/static/js/grid.js
@@ -1,28 +1,66 @@
-let g_width, g_height, g_scene, g_camera, g_renderer, g_settings, g_gui;
+const HEADER_OFFSET = 64;
+
+let g_width,
+	g_height,
+	g_scene,
+	g_camera,
+	g_renderer,
+	g_settings,
+	g_gui,
+	g_imageMeshes,
+	g_selectedImage;
 
 g_settings = {
 	progress: 0
 };
+
+g_imageMeshes = [];
 
 const vertexShader = `
 uniform float uTime;
 uniform float uProgress;
 uniform vec2 uResolution;
 uniform vec2 uQuadSize;
+uniform vec4 uCorners;
 
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec2 vSize;
 
+float rand(vec2 n) { 
+	return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+
+float noise(vec2 p) {
+	vec2 ip = floor(p);
+	vec2 u = fract(p);
+	u = u*u*(3.0-2.0*u);
+	
+	float res = mix(
+		mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
+		mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
+	return res*res;
+}
+
 void main() {
     vUv = uv;
     vec4 thumbState = modelMatrix * vec4(position, 1.0);
     vec4 fullState = vec4(position, 1.0);
-    // fullState.x *= uQuadSize.x;
-    // fullState.y *= uQuadSize.y;
     fullState.x *= uResolution.x;
-    fullState.y *= uResolution.y;
-    vec4 finalState = mix(thumbState, fullState, uProgress);
+    fullState.y *= uResolution.y * 0.8 + 64.0;
+	fullState.y += uResolution.y * 0.1 * uProgress;
+	fullState.z += uCorners.x;
+	float cornersProgress = mix(
+        mix(uCorners.z,uCorners.w,uv.x),
+        mix(uCorners.x,uCorners.y,uv.x),
+        uv.y
+    );
+	// fullState.x *= 2.0;
+	// fullState.x *= 2.0;
+    vec4 finalState = mix(thumbState, fullState, cornersProgress);
+	// finalState.x += uProgress * noise(vec2(position.x, position.y) * 2.0) * 100.0;
+	// finalState.y += uProgress * noise(vec2(position.x, position.y) * 2.0) * 100.0;
+	vSize = mix(uQuadSize, uResolution, cornersProgress);
     gl_Position = projectionMatrix * viewMatrix * finalState;
 }
 `;
@@ -30,15 +68,38 @@ void main() {
 const fragmentShader = `
 uniform float uTime;
 uniform float uProgress;
+uniform float uOpacity;
 uniform sampler2D uTexture;
+uniform vec2 uQuadSize;
 
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec2 vSize;
 
+vec2 getUv() {
+	vec2 tempUv = vUv - vec2(0.5);
+	float vAspect = vSize.x / vSize.y;
+	float tAspect = uQuadSize.x / uQuadSize.y;
+	if (vAspect > tAspect) {
+		tempUv *= vec2(vAspect / tAspect, 1.0);
+	} else {
+		tempUv *= vec2(1.0, tAspect / vAspect);
+	}
+	tempUv += vec2(0.5);
+	return tempUv;
+}
+
 void main() {
-    vec4 image = texture2D(uTexture, vUv);
-    gl_FragColor = image;
+	vec2 uv = getUv();
+	if (uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0) {
+		// gl_FragColor = vec4(1.0, 1.0, 1.0, 0.0);
+		discard;
+	} else if (uOpacity > 0.0) {
+		vec4 image = texture2D(uTexture, uv);
+		gl_FragColor = mix(image, vec4(0.96, 0.96, 0.96, 1.0), 1.0 - uOpacity);
+	} else {
+		discard;
+	}
 }
 `;
 
@@ -62,20 +123,20 @@ function setup() {
 	}
 
 	syncImages();
+	// captureClickEvents();
 	onResize();
-
 	animate();
 }
 
-function createShaderMaterial(texture) {
+function createShaderMaterial(imageUrl) {
 	const uniforms = {
 		uTime: { value: 1.0 },
 		uProgress: { value: 0.0 },
 		uCorners: { value: new THREE.Vector4(0, 0, 0, 0) },
-		uTexture: { value: new THREE.TextureLoader().load(texture) },
-		uTextureSize: { value: new THREE.Vector2(100, 100) },
+		uTexture: { value: new THREE.TextureLoader().load(imageUrl) },
 		uResolution: { value: new THREE.Vector2(g_width, g_height) },
-		uQuadSize: { value: new THREE.Vector2(300, 300) } // Size of the plane
+		uQuadSize: { value: new THREE.Vector2(300, 300) }, // Size of the plane
+		uOpacity: { value: 1.0 }
 	};
 	const material = new THREE.ShaderMaterial({
 		uniforms,
@@ -92,43 +153,85 @@ function syncImages() {
 	for (const image of images) {
 		const bounds = image.getBoundingClientRect();
 		image._x = bounds.left + bounds.width / 2 - g_width / 2;
-		image._y = -(bounds.top + bounds.height / 2) + g_height / 2 - window.scrollY;
+		image._y = -(bounds.top + bounds.height / 2) + g_height / 2 - window.scrollY + HEADER_OFFSET;
 
 		if (!image._mesh) {
-			const texture = new THREE.TextureLoader().load(image.src);
 			const geometry = new THREE.PlaneBufferGeometry(1, 1, 30, 30);
 			const material = createShaderMaterial(image.src);
 			const plane = new THREE.Mesh(geometry, material);
 			plane.scale.set(300, 300, 1);
 			g_scene.add(plane);
 			image._mesh = plane;
+			plane._image = image;
+			g_imageMeshes.push(plane);
 		}
 
 		image._mesh.position.set(image._x, image._y + window.scrollY, 0);
-
-		// plane.position.set(image._x, image._y, 0);
-		// plane.scale.set(300, 300, 1);
-		// g_scene.add(plane);
-		// image._mesh = plane;
+		image._mesh.material.uniforms.uResolution.value = new THREE.Vector2(g_width, g_height);
 	}
 }
 
+function captureClickEvents() {
+	// const links = document.querySelectorAll('.students-grid a');
+	// for (const link of links) {
+	// 	link.addEventListener('click', function (e) {
+	// 		// e.preventDefault();
+	// 		g_selectedImage = this.querySelector('img');
+	// 		// g_settings.progress = progress;
+	// 		// let uCorners = g_selectedImage._mesh.material.uniforms.uCorners.value;
+	// 		// gsap.set(g_settings, { progress: 0 });
+	// 		// gsap.to(g_settings, { progress: 1, duration: 1, ease: 'power2.out' });
+	// 		// gsap
+	// 		// 	.timeline()
+	// 		// 	.to(uCorners, { x: 1 })
+	// 		// 	.to(uCorners, { y: 1 }, '-=0.4')
+	// 		// 	.to(uCorners, { z: 1 }, '-=0.4')
+	// 		// 	.to(uCorners, { w: 1 }, '-=0.4');
+	// 	});
+	// }
+	barba.init({
+		transitions: [
+			{
+				name: 'default-transition',
+				leave(data) {
+					// return gsap.timeline().to(data.current.container, { opacity: 0, duration: 1.0 });
+					const { trigger } = data;
+					g_selectedImage = trigger.querySelector('img');
+					console.log(data);
+					let uCorners = g_selectedImage._mesh.material.uniforms.uCorners.value;
+					gsap.set(g_settings, { progress: 0 });
+					gsap
+						.timeline()
+						.to(uCorners, { x: 1 })
+						.to(uCorners, { y: 1 }, '-=0.4')
+						.to(uCorners, { z: 1 }, '-=0.4')
+						.to(uCorners, { w: 1 }, '-=0.4');
+					return gsap.to(g_settings, { progress: 1, duration: 1, ease: 'power2.out' });
+					// create your stunning leave animation here
+				},
+				enter() {
+					return gsap.timeline().from(data.next.container, { opacity: 0, duration: 1.0 });
+					// create your amazing enter animation here
+				}
+			}
+		]
+	});
+}
+
 function onScroll() {
-	const images = document.querySelectorAll('.students-grid img');
-	for (const image of images) {
-		// console.log(image._mesh.position);
-		if (!image._mesh) continue;
-		// const bounds = image.getBoundingClientRect();
-		image._mesh.position.set(image._x, image._y + window.scrollY, 0);
-	}
+	g_imageMeshes.forEach(function (mesh) {
+		mesh.position.set(mesh._image._x, mesh._image._y + window.scrollY, 0);
+	});
 }
 
 function animate() {
 	requestAnimationFrame(animate);
-	const images = document.querySelectorAll('.students-grid img');
-	for (const image of images) {
-		if (!image._mesh) continue;
-		image._mesh.material.uniforms.uProgress.value = g_settings.progress;
+	for (const mesh of g_imageMeshes) {
+		mesh.material.uniforms.uOpacity.value = 1.0 - g_settings.progress;
+	}
+	if (g_selectedImage && g_selectedImage._mesh) {
+		g_selectedImage._mesh.material.uniforms.uProgress.value = g_settings.progress;
+		g_selectedImage._mesh.material.uniforms.uOpacity.value = 1.0;
 	}
 	g_renderer.render(g_scene, g_camera);
 }
